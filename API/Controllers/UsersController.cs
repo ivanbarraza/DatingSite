ed440c701291b1c9e4e6592d036ce354b5044ccc
Ivan Barraza <ivan.developer@yahoo.com>
2024-01-26 22:06:29 -0700

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    public UsersController(DataContext context)
+    {
+        Context = context;
+    }
+
+    public DataContext Context { get; }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var lstUsers = await Context.Users.ToListAsync();
+
+        return lstUsers;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await Context.Users.FindAsync(id);
+
+        return user;
+    }
+}
